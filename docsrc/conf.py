048059eee8e637a06942f5bbbc5c178cdaebb4bd
LmR <letmer00t@gmail.com>
2022-07-18 15:52:58 +0200

    Update new version of the project

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, LetMeR00t"
 author = "LmR"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.0"
+release = "0.7.0"
 
 
 # -- General configuration ---------------------------------------------------
