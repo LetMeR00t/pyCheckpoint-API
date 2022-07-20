59f5f04ac63340f12560c9befbe2702665b7c6b4
LmR <letmer00t@gmail.com>
2022-07-20 17:37:01 +0200

    ci: prepare release for new version

--- pycheckpoint_api/__init__.py
+++ pycheckpoint_api/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "LmR <letmer00t@gmail.com>"
 __license__ = "MIT"
 __contributors__ = []
-__version__ = "0.7.0"
+__version__ = "1.0.0"
 
 from pycheckpoint_api.firewallManagement import FirewallManagement  # noqa
